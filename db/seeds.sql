0c5a19e20fc6582329dc14f644f3512a6796b944
k23kumar <k23kumar@gmail.com>
2018-08-22 19:37:03 -0700

    initial files

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+use burgers_db;
+
+INSERT into burgers (burger_name, devoured) VALUES ("Cheese Burger", false);
+
+INSERT into burgers (burger_name, devoured) VALUES ("Turkey Burger", false);
+
+INSERT into burgers (burger_name, devoured) VALUES ("Elk Burger", false);
